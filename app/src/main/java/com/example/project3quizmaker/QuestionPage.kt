f36979b2613f0d228ff8987b05d1bb09900abbb4
petcarmi <petcarmi@iu.edu>
2023-09-26 20:01:15 -0400

    finished project 4

--- app/src/main/java/com/example/project3quizmaker/QuestionPage.kt
+++ app/src/main/java/com/example/project3quizmaker/QuestionPage.kt
@@ -1,11 +1,14 @@
 package com.example.project3quizmaker
 
 import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 lateinit var imageView: ImageView
@@ -18,7 +21,7 @@ class QuestionPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_page)
-
+        var mp=MediaPlayer()
         imageView=findViewById(R.id.imageview1)
         imageView.setImageResource(R.drawable.hated_math_1200x627)
         /*
@@ -48,6 +51,7 @@ class QuestionPage : AppCompatActivity() {
         val leftNumber = generateNumber(difficulty)
         val rightNumber = generateNumber(difficulty)
         val correctAnswer = solveProblem(operation, leftNumber, rightNumber)
+        val write: TextView = findViewById(R.id.toast)
 
         leftText.text = leftNumber.toString()
         rightText.text = rightNumber.toString()
@@ -64,17 +68,32 @@ class QuestionPage : AppCompatActivity() {
             val yourAnswer = Integer.parseInt(editText.text.trim().toString())
             if (yourAnswer == correctAnswer) {
                 correctAnswers = correctAnswers?.inc()
+                write.text="good job"
+                mp.setDataSource(this, Uri.parse("android.resource://"+this.packageName+"/"+R.raw.good))
+                mp.prepare()
+                mp.start()
+                val toast = Toast.makeText(this, "Correct. Good work!", Toast.LENGTH_SHORT)
+                toast.show()
             } else {
                 wrongAnswers = wrongAnswers?.inc()
+                write.text="wrong"
+                mp.setDataSource(this, Uri.parse("android.resource://"+this.packageName+"/"+R.raw.bad))
+                mp.prepare()
+                mp.start()
+                val toast = Toast.makeText(this, "Wrong.", Toast.LENGTH_SHORT)
+                toast.show()
+
+
             }
             /*
             adds to intent folder so we have relevant information stored
      */
             val totalAnswers = correctAnswers!! + wrongAnswers!!
             if (totalAnswers >= totalProblems!!) {
-                val intent = Intent(this, ResultPage::class.java)
+                val intent = Intent(this, MainActivity::class.java)
                 intent.putExtra("totalProblems", totalProblems)
                 intent.putExtra("correctAnswers", correctAnswers)
+                intent.putExtra("operation",operation)
                 startActivity(intent)
             } else {
                 val intent = Intent(this, QuestionPage::class.java)
